f1079c37e0e510e5afcab7e399c9a6033653d87e
Mark-Andre17 <89165035680@mail.ru>
2024-05-01 21:11:50 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -21,3 +21,56 @@ document.querySelectorAll(".burger").forEach(function(button) {
       });
     });
   });
+// слайдер галереи + точки слайдера галереи
+let buttonNext = document.querySelector(".gallery-next");
+let buttonPrev = document.querySelector(".gallery-prev");
+let gallerySlider = document.querySelector(".gallery-slider");
+let gallerySliderLine = document.querySelector('.projects-gallery');
+let galleryItems = document.querySelectorAll(".projects-gallery-item");
+let galleryDots = document.querySelectorAll(".dot-slider");
+let galleryOffset = 0;
+let count = 0;
+buttonNext.addEventListener("click", function() {
+  if(count < galleryItems.length - 1) {
+    count += 1;
+    gallerySlider.scrollLeft += gallerySlider.offsetWidth;
+    galleryDots[count].classList.add("active-dot");
+    galleryDots[count - 1].classList.remove("active-dot");
+  }else{
+    count = 0;
+    gallerySlider.scrollLeft = 0;
+    galleryDots[count].classList.add("active-dot");
+    galleryDots[galleryItems.length - 1].classList.remove("active-dot");
+  }
+});
+buttonPrev.addEventListener("click", function() {
+  if(count > 0) {
+    count -= 1;
+    gallerySlider.scrollLeft -= gallerySlider.offsetWidth;
+    galleryDots[count].classList.add("active-dot");
+    galleryDots[count + 1].classList.remove("active-dot");
+  }else{
+    count = galleryItems.length - 1;
+    gallerySlider.scrollLeft = gallerySliderLine.offsetWidth - gallerySlider.offsetWidth;
+    galleryDots[count].classList.add("active-dot");
+    galleryDots[0].classList.remove("active-dot");
+  }
+});
+// слайдер персонала автоматический
+let testimonialSlider = document.querySelector(".testimonial-slider");
+let testimonialSliderLine = document.querySelector('.testimonial-wrapper');
+console.log(testimonialSlider);
+console.log(testimonialSliderLine.offsetWidth);
+
+function autoSlider() {
+  let offset = 0;
+  setInterval(function() {
+    offset = offset + testimonialSlider.offsetWidth;
+    testimonialSliderLine.style.transform = "translateX(-" + offset + "px)";
+    if(offset > (testimonialSliderLine.offsetWidth - testimonialSlider.offsetWidth)) {
+      offset = 0;
+      testimonialSliderLine.style.transform = "translateX(-" + offset + "px)";
+    }
+  },5000)
+}
+autoSlider();
